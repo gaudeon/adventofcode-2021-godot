8a8da165a23c8909033e2cb2bf30615da80609d1
Travis Chase <travis.chase@gmail.com>
2021-12-20 03:35:16 -0700

    finished day 2 part 1
    note: takes roughly 16 minutes to finish :)

--- day_2/day_2.gd
+++ day_2/day_2.gd
@@ -4,6 +4,8 @@ signal move_forward
 signal move_up
 signal move_down
 
+var file_location = "res://day_2/day2_input.real.tres"
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -11,11 +13,20 @@ signal move_down
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	emit_signal("move_forward", 1)
-	emit_signal("move_down", 2)
-	emit_signal("move_up", 3)
+	var data = _load_file().split("\n",false)
+	for i in range(data.size()):
+		print(data[i])
+		var pair = data[i].split(" ",false)
+		var command = pair[0]
+		var amount = pair[1] as int
 
+		if(command == "forward"):
+			emit_signal("move_forward", amount)
+		elif(command == "down"):
+			emit_signal("move_down", amount)
+		elif(command == "up"):
+			emit_signal("move_up", amount)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _load_file():
+	var fileio = load("res://shared/fileio.gd").new()
+	return fileio.load_file(file_location)
--- day_2/submarine.gd
+++ day_2/submarine.gd
@@ -8,10 +8,11 @@ var move_queue = []
 
 var tween_running = false
 
+export (Vector2) var relativePosition = Vector2()
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,10 +28,13 @@ func _process(delta):
 		var target = Vector2(self.position.x, self.position.y)
 		if (event["action"] == "forward"):
 			target.x += event["amount"] * speed
+			relativePosition.x += event["amount"]
 		elif (event["action"] == "up"):
 			target.y -= event["amount"] * speed
+			relativePosition.y -= event["amount"]
 		elif (event["action"] == "down"):
 			target.y += event["amount"] * speed
+			relativePosition.y += event["amount"]
 			
 		var tween = get_node("Tween")
 		tween.interpolate_property(self, "position",
@@ -61,6 +65,20 @@ func _on_day_2_move_up(amount):
 		"amount": amount
 	})
 
+func _mark_position():
+	var label = PositionLabel.new(self.position, relativePosition)
+	
+	get_parent().add_child(label)
 
 func _on_Tween_tween_all_completed():
+	_mark_position()
 	tween_running = false
+	
+class PositionLabel:
+	extends Label
+	
+	func _init(displayPosition, descriptivePosition):
+		self.set_position(displayPosition)
+		self.text = "(" + descriptivePosition.x as String + ", " + descriptivePosition.y as String + ")"
+
+
